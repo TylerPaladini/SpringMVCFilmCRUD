2437c1d230d362ecc3c3e4c21a50ae9b14cf98ce
Tyler Paladini <TylerPaladini@yahoo.com>
2018-10-12 15:34:03 -0600

    DAO update

--- src/main/java/com/skilldistillery/film/data/DatabaseAccessor.java
+++ src/main/java/com/skilldistillery/film/data/DatabaseAccessor.java
@@ -1,5 +1,13 @@
 package com.skilldistillery.film.data;
 
+import java.util.List;
+
+import com.skilldistillery.film.entities.Film;
+
 public interface DatabaseAccessor {
+	
+	public Film filmById();
+	public List<Film> getFilmByKeyword(String keyword);
+	public Film addFilm(Film film);
 
 }
